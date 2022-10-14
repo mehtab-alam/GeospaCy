bd42b5b94d9c8bcbb97c0e8e5fd91981a69e04b2
mehtab-alam <mehtabalamshah@gmail.com>
2022-10-14 10:28:31 +0200

    Add files via upload

--- 1_🏠_Parser.py
+++ 1_🏠_Parser.py
@@ -126,10 +126,9 @@ def set_selected_entities(doc):
 
 def extract_spatial_entities(text):
     #nlp = en_core_web_md.load()
-    nlp = spacy.load(model)
         
     try:
-        
+        nlp = spacy.load(model)
         nlp.add_pipe("spatial_pipeline", after="ner")
         
         doc = nlp(text)
